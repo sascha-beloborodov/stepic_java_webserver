1eff1bd5e5d3ba2ed97b79e810333af9384aa8b4
Sascha Beloborodov <a.beloborodov@hotmail.com>
2016-02-14 17:07:52 +0300

    home work #2 completed

---
+++ L2.1 Authorization/src/main/java/servlets/SignInServlet.java
@@ -0,0 +1,49 @@
+package servlets;
+
+/**
+ * Created by sascha on 13.02.16.
+ */
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class SignInServlet extends HttpServlet {
+    private final AccountService accountService;
+
+    public SignInServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (profile == null || !profile.getPass().equals(pass)) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().print("Unauthorized");
+            return;
+        }
+
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().print("Authorized: " + login);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+}
---
+++ L2.1 Authorization/src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,49 @@
+package servlets;
+
+/**
+ * Created by sascha on 13.02.16.
+ */
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class SignUpServlet extends HttpServlet {
+    private final AccountService accountService;
+
+    public SignUpServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (profile != null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile newProfile = new UserProfile(login, pass, "mail@mail.ru");
+        accountService.addNewUser(newProfile);
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().print("User created");
+    }
+}
